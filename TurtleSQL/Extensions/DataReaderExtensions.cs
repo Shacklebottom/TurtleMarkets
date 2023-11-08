420495cd9453d1734a6abbdc0a11566d8722d0f9
Shacklebottom <last0illusion@gmail.com>
2023-11-08 01:23:08 -0600

    missed a couple uncleaned using statements

--- TurtleSQL/Extensions/DataReaderExtensions.cs
+++ TurtleSQL/Extensions/DataReaderExtensions.cs
@@ -1,10 +1,4 @@
 ﻿using Microsoft.Data.SqlClient;
-using System;
-using System.Collections.Generic;
-using System.Data.SqlTypes;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TurtleSQL.Extensions
 {
